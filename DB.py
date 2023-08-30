2d75771f6584034ccd9a54e6229840147e8536bd
chipigol <guidocasella@hotmail.com>
2023-08-29 23:09:16 -0300

    feature create stocks table and fixed connection

--- DB.py
+++ DB.py
@@ -1,6 +1,5 @@
-from sqlalchemy import create_engine, Column, Float, Integer, String, Date
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+import psycopg2
+from psycopg2 import sql
 from getpass import getpass
 
 # Database connection parameters
@@ -8,38 +7,44 @@ dbname = "data-engineer-database"
 user = "guidocasella_coderhouse"
 host = "data-engineer-cluster.cyhh5bfevlmn.us-east-1.redshift.amazonaws.com"
 port = "5439"  # Default port for Redshift
-
-# Prompt for password securely
 password = getpass("Enter your password: ")
 
-# Construct the connection URL
-connection_url = f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{dbname}"
-
-# Define the table using SQLAlchemy's declarative_base
-Base = declarative_base()
-
-class Stocks(Base):
-    __tablename__ = 'stocks'
-    __table_args__ = {'schema': 'guidocasella_coderhouse'}
-    
-    id = Column(Integer, primary_key=True)
-    open = Column(Float)
-    high = Column(Float)
-    low = Column(Float)
-    close = Column(Float)
-    volume = Column(Integer)
-    dividends = Column(Float)
-    stock_splits = Column(Float)
-    date = Column(Date)
+# Construct the connection string
+connection_string = f"dbname='{dbname}' user='{user}' host='{host}' port='{port}' password='{password}'"
 
 try:
-    # Create an engine
-    engine = create_engine(connection_url)
+    # Establish the connection
+    connection = psycopg2.connect(connection_string)
 
-    # Create the table
-    Base.metadata.create_all(engine)
+    # Create a cursor
+    cursor = connection.cursor()
 
-    print("Table 'Stocks' created successfully.")
+    # Define the create table query
+    create_table_query = sql.SQL("""
+        CREATE TABLE guidocasella_coderhouse.STOCKS (
+            id INT PRIMARY KEY,
+            "open" FLOAT,
+            high FLOAT,
+            low FLOAT,
+            close FLOAT,
+            volume INT,
+            dividends FLOAT,
+            stock_splits FLOAT,
+            date DATE
+        )
+    """)
 
-except Exception as e:
+    # Execute the create table query
+    cursor.execute(create_table_query)
+
+    # Commit the changes
+    connection.commit()
+
+    print("Table 'STOCKS' created successfully.")
+
+    # Close the cursor and connection
+    cursor.close()
+    connection.close()
+
+except psycopg2.Error as e:
     print("Error:", e)
