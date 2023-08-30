a93d6921c7e09cf5d6e5e368dc19ee940645018f
chipigol <guidocasella@hotmail.com>
2023-08-29 21:55:04 -0300

    feature analyze data/sanity check

--- ETL.py
+++ ETL.py
@@ -1,4 +1,5 @@
 import yfinance as yf
+import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
 
 # List of company tickers
@@ -17,8 +18,20 @@ for ticker in company_tickers:
     data = company.history(start=start_date, end=end_date)
     company_data[ticker] = data
 
-# Print retrieved data for each company
-for ticker, data in company_data.items():
-    print(f"Company: {ticker}")
-    print(data)
-    print("\n")
+# Create subplots for each company
+fig, axs = plt.subplots(len(company_tickers), 1, figsize=(10, 6 * len(company_tickers)))
+
+# Plot Close values for each company
+for idx, (ticker, data) in enumerate(company_data.items()):
+    ax = axs[idx]
+    ax.plot(data.index, data['Close'], label=ticker)
+    ax.set_title(f"Close Value Evolution - {ticker}")
+    ax.set_xlabel("Date")
+    ax.set_ylabel("Close Value")
+    ax.legend()
+
+# Adjust layout
+plt.tight_layout()
+
+# Show the plots
+plt.show()
